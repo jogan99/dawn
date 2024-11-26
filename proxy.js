a8aeb6e5a5ed709ec1a5fa169c11f4209f658a91
0xjogan <82117196+jogan99@users.noreply.github.com>
2024-11-26 14:05:36 +0800

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,12 @@
+module.exports = [
+    'http://wlnegzqk:eqtupejbsnfl@207.244.217.165:6712',
+    'http://wlnegzqk:eqtupejbsnfl@107.172.163.27:6543',
+    'http://wlnegzqk:eqtupejbsnfl@64.137.42.112:5157',
+    'http://wlnegzqk:eqtupejbsnfl@173.211.0.148:6641',
+    'http://wlnegzqk:eqtupejbsnfl@161.123.152.115:6360',
+    'http://wlnegzqk:eqtupejbsnfl@167.160.180.203:6754',
+    'http://wlnegzqk:eqtupejbsnfl@154.36.110.199:6853',
+    'http://wlnegzqk:eqtupejbsnfl@173.0.9.70:5653',
+    'http://wlnegzqk:eqtupejbsnfl@173.0.9.209:5792',
+	// change all this with your proxy if you want to use proxy
+];
